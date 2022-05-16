3798ad3b643522ae42cf57489ec8ca8a5dae3840
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-05-16 09:58:21 +0300

    Resized my images

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -14,12 +14,12 @@ document.getElementById("form1").onsubmit=function(e) {
 
     document.getElementById("grade").innerHTML = result;
 
-    if (result == 0) {result2 = "You really need to put more effort.<br /><img src='../Assets/Images/face1.jpeg' width='300' />"};
-    if (result == 20) {result2 = "You can do better than this with consistent effort.<br /><img src='../Assets/Images/face2.jpeg' width='300' />"};
-    if (result == 40) {result2 = "Nice try.You can better your score.<br /><img src='../Assets/Images/face3.jpg' width='300' />"};
+    if (result == 0) {result2 = "You really need to put more effort.<br /><img src='../Assets/Images/face1.jpeg' width='200' />"};
+    if (result == 20) {result2 = "You can do better than this with consistent effort.<br /><img src='../Assets/Images/face2.jpeg' width='200' />"};
+    if (result == 40) {result2 = "Nice try.You can better your score.<br /><img src='../Assets/Images/face3.jpg' width='200' />"};
     if (result == 60) {result2 = "Average.<br /><img src='./Assets/Images/face3.jpg' width='300' />"};
-    if (result == 80) {result2 = "Nice try.Awesome.<br /><img src='../Assets/Images/face2.jpeg' width='300' />"};
-    if (result == 100) {result2 = "You did really great.Amazing.<br /><img src='../Assets/Images/face3.jpg' width='300' />"};
+    if (result == 80) {result2 = "Nice try.Awesome.<br /><img src='../Assets/Images/face2.jpeg' width='200' />"};
+    if (result == 100) {result2 = "You did really great.Amazing.<br /><img src='../Assets/Images/face3.jpg' width='200' />"};
 
 
     document.getElementById("grade2").innerHTML = result2;
